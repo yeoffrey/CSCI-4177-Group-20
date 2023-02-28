1d6883b480d03f3b922075b8e60052f80fda4ed6
dhamodaran <dhamodaran@cs.dal.ca>
2023-02-28 11:51:14 -0400

    added basic google apis algo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function BookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
+      .then(response => response.json())
+      .then(data => {
+        setBooks(data.items.map(book => {
+          return {
+            title: book.volumeInfo.title,
+            cover: book.volumeInfo.imageLinks.thumbnail,
+            price: book.saleInfo.listPrice.amount
+          };
+        }));
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.title}>
+            <img src={book.cover} alt={book.title} />
+            <h2>{book.title}</h2>
+            <p>{book.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default BookList;
