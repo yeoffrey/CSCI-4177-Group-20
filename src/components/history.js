1b495457afcc53f7e303e39217bc4b85983a0e35
wiedemann <br664676@dal.ca>
2023-03-30 13:49:30 -0300

    pseudocode for dynamic history page

--- src/components/history.js
+++ src/components/history.js
@@ -4,6 +4,78 @@ import book from '../images/book.svg';
 import page from '../images/file-earmark-break.svg';
 import clock from '../images/clock.svg';
 function History() {
+    const booksRead = [];
+    const totalBooks = 0;
+    const totalPages = 0;
+    const timeSpent = 0;
+    /*if (database query returns 0 items){
+        return (
+            <div className="App">
+                <div id="Content-Header" className="container p-2 my-1 text-white bg-secondary">
+                    <div className="row">
+                        <div className="col">
+                            <img src={require('../images/pfp.jpg')} className="img-fluid" alt=""/>
+                        </div>
+                        <div className="col-9">
+                            <h1>$(USERNAME)'s Statistics</h1>
+                            <div className="container p-2 my-1 text-white">
+                                <div className="row">
+                                    <div className="col">
+                                        <h1><img src={book} className="img-fluid" alt=""/></h1>
+                                        <h2>0 Books read</h2>
+                                    </div>
+                                    <div className="col">
+                                        <h1><img src={page} className="img-fluid" alt=""/></h1>
+                                        <h2>0 Pages read</h2>
+                                    </div>
+                                    <div className="col">
+                                        <img src={clock} className="img-fluid" alt=""/>
+                                        <h2>Approx. 0 Hours spent reading</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="Content-Main" className="container bg-secondary">
+                    <div className="d-flex justify-content-between">
+                        <h1>Previous Reads:</h1>
+                        <Link type="button" className="btn btn-info" to='/addbook'><h1>+</h1></Link>
+                    </div>
+                    <div id="History" className="row p-5 text-center ">
+                        <h3>No Previous books associated with this account.</h3>
+                        <h5>You can add books you have read on your own time by pressing the
+                            "+" icon in the top right.</h5>
+                    </div>
+                </div>
+            </div>
+        );
+    }*/
+    /*else {
+        for (let i = 0;i<databaseEntries; i++) {
+        const book = ${APIQuery.${databaseQuery.isbn}}
+            booksRead[i] = (
+                <div className="card">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col">
+                                <img src={require(${book.imageLinks.thumbnail})} className="img-thumbnail img-fluid" width="75%" alt=""/>
+                            </div>
+                            <div className="col-10 text-start">
+                                <h5 className="book-title">${book.title}</h5>
+                                <h5 className="book-title">${book.averageRating}</h5>
+                                <h5 className="book-title">${book.pageCount}</h5>
+                                <p className="card-text">${book.description}</p>
+                                <button className="btn btn-primary" linkTO=${book.infolink} >Go to book page</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>);
+            totalBooks +=1;
+            totalPages += book.pageCount;
+        }
+        timeSpent = Math.round((totalPages * 2) / 60)
+    }*/
     return (
         <div className="App">
             <div id="Content-Header" className="container p-2 my-1 text-white bg-secondary">
@@ -17,15 +89,15 @@ function History() {
                             <div className="row">
                                 <div className="col">
                                     <h1><img src={book} className="img-fluid" alt=""/></h1>
-                                    <h2>3 Books read</h2>
+                                    <h2>{/*${totalBooks}*/} 3 Books read</h2>
                                 </div>
                                 <div className="col">
                                     <h1><img src={page} className="img-fluid" alt=""/></h1>
-                                    <h2>1388 Pages read</h2>
+                                    <h2>{/*${totalPages}*/} 1388 Pages read</h2>
                                 </div>
                                 <div className="col">
                                     <img src={clock} className="img-fluid" alt=""/>
-                                    <h2>Approx. 46 Hours spent reading</h2>
+                                    <h2>Approx. {/*${timeSpent}*/} 46 Hours spent reading</h2>
                                 </div>
                             </div>
                         </div>
@@ -38,6 +110,9 @@ function History() {
                     <Link type="button" className="btn btn-info" to='/addbook'><h1>+</h1></Link>
                 </div>
                 <div id="History" className="row p-5 text-center ">
+                    {/*for (let i = 0; i < booksRead.length(); i++){
+                        print(booksRead[i])
+                    }*/}
                     <div className="card">
                         <div className="card-body">
                             <div className="row">
