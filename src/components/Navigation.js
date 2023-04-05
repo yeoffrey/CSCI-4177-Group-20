020111656e543ee5f1e7aec34db5567e69d8b42c
wiedemann <br664676@dal.ca>
2023-04-05 19:05:22 -0300

    fixed navBar reliability

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,10 @@
 import React, {useState} from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
     const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-    const [show, setShow] = useState(false);
-    const showDropdown = (e)=>{
-        setShow(!show);
-    }
-    const hideDropdown = e => {
-        setShow(false);
-    }
     return (
         <div className="navigation">
             <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -21,31 +14,15 @@ function Navigation() {
                     </NavLink>
                     <div>
                         <ul className="navbar-nav ml-auto">
-                            <Nav onSelect={handleSelect} >
+                            <Nav onSelect={handleSelect}>
                                 <Nav.Item>
                                     <NavLink className="nav-link" to="/">
                                         Home
                                     </NavLink>
                                 </Nav.Item>
-                                <NavDropdown title="Account" id="nav-dropdown" menuVariant="dark"
-                                             show={show}
-                                             onMouseEnter={showDropdown}
-                                             onMouseLeave={hideDropdown}>
-                                    <NavDropdown.Item>
-                                        <NavLink className="nav-link" to="/profile">
-                                            Profile
-                                        </NavLink>
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item>
-                                        <NavLink className="nav-link" to="/history">
-                                            History & Stats
-                                        </NavLink>
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item>
-                                        <NavLink className="nav-link" to="/addBook">
-                                            Add Book
-                                        </NavLink>
-                                    </NavDropdown.Item>
+                                <NavDropdown title="Account" id="navigation-dropdown" menuVariant="dark">
+                                    <NavDropdown.Item to="/profile" as={Link}>Profile</NavDropdown.Item>
+                                    <NavDropdown.Item to="/history" as={Link}>History</NavDropdown.Item>
                                 </NavDropdown>
                                 <NavLink className="nav-link" to="/login">
                                     Login
